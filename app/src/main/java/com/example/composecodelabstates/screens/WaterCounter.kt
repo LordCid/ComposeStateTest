b701131c8ead17176c341633390461b98e08473f
DESKTOP-7FRSP0K\Albert <acidbjbo@gmail.com>
2023-07-26 13:34:59 +0200

    Added button and string resources

--- app/src/main/java/com/example/composecodelabstates/screens/WaterCounter.kt
+++ app/src/main/java/com/example/composecodelabstates/screens/WaterCounter.kt
@@ -1,16 +1,28 @@
 package com.example.composecodelabstates.screens
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.composecodelabstates.R
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier){
-    val count = 0
-    Text(
-        text = "You've had $count glasses",
+    var count = 0
+    Column(
         modifier = modifier.padding(16.dp)
-    )
+    ) {
+        Text(stringResource(R.string.glasses_counter_label, count))
+        Button(
+            onClick = { count ++  },
+            modifier = Modifier.padding(top = 8.dp)
+        ) {
+            Text(stringResource(R.string.add_one_button_label))
+        }
+    }
+
 }
--- app/src/main/java/com/example/composecodelabstates/screens/WellnessScreen.kt
+++ app/src/main/java/com/example/composecodelabstates/screens/WellnessScreen.kt
@@ -1,2 +1,9 @@
 package com.example.composecodelabstates.screens
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier){
+    WaterCounter(modifier)
+}
