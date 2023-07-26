a5ebaf81c0348cee74d246dc59accfa7269a923a
DESKTOP-7FRSP0K\Albert <acidbjbo@gmail.com>
2023-07-26 13:58:05 +0200

    rember implemente

--- app/src/main/java/com/example/composecodelabstates/screens/WaterCounter.kt
+++ app/src/main/java/com/example/composecodelabstates/screens/WaterCounter.kt
@@ -5,6 +5,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -12,13 +16,13 @@ import com.example.composecodelabstates.R
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier){
-    var count = 0
+    var count by remember { mutableStateOf(0) }
     Column(
         modifier = modifier.padding(16.dp)
     ) {
         Text(stringResource(R.string.glasses_counter_label, count))
         Button(
-            onClick = { count ++  },
+            onClick = { count++  },
             modifier = Modifier.padding(top = 8.dp)
         ) {
             Text(stringResource(R.string.add_one_button_label))
