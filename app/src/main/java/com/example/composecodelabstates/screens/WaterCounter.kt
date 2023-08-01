3a375ecbd1b18331b2e1d531f85137a857d25040
DESKTOP-7FRSP0K\Albert <acidbjbo@gmail.com>
2023-08-01 10:53:52 +0200

    StatefulCounter state elevantion example implemented

--- app/src/main/java/com/example/composecodelabstates/screens/WaterCounter.kt
+++ app/src/main/java/com/example/composecodelabstates/screens/WaterCounter.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -18,7 +19,7 @@ import com.example.composecodelabstates.R
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier){
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember { mutableStateOf(0) }
+        var count by rememberSaveable { mutableStateOf(0) }
         if(count > 0){
             Text(stringResource(R.string.glasses_counter_label, count))
         }
@@ -32,7 +33,43 @@ fun WaterCounter(modifier: Modifier = Modifier){
     }
 
 }
-//
+
+@Composable
+fun StatelessCounter(
+    count: Int,
+    onIncrement: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier.padding(16.dp)) {
+        if (count > 0) {
+            Text(text = stringResource(R.string.glasses_counter_label, count))
+        }
+        Button(
+            modifier = Modifier.padding(top = 8.dp),
+            onClick = onIncrement,
+            enabled = count < 10
+        ) {
+            Text(text = stringResource(R.string.add_one_button_label))
+        }
+
+    }
+}
+
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier){
+    var count by remember { mutableStateOf(0) }
+
+    Column(
+        modifier = Modifier.padding(start = 8.dp),
+    ) {
+        val modifier = Modifier.padding(bottom = 8.dp)
+        StatelessCounter(count, { count++ }, modifier)
+        StatelessCounter(count, { count *= 2 }, modifier)
+    }
+
+}
+
+
 //@Composable
 //fun WaterCounter(modifier: Modifier = Modifier){
 //    Column(modifier = modifier.padding(16.dp)) {
